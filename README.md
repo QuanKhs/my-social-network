bae1d43c7c3f889743c301b0e90df8013490fffa
QuanKhs <quanphamluong@gmail.com>
2021-05-06 19:29:32 +0700

    initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+# my-social-network using Nodejs MongoDB Express React
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Social Network</h1>
+    </div>
+  );
+}
+
+export default App;
